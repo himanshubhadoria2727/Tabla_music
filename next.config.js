9f59a9ba4482ee01bf0c1a1e61cbd98ac955a82c
himanshubhadoria2727 <hbhadoria78@gmail.com>
2024-08-29 22:23:52 +0530

    #for images on live

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost','main--tabla-admin.netlify.app'],
   },
   webpack(config) {
     config.module.rules.push({
